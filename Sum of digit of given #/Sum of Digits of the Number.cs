51e01225c0861bdeb5e56c14ebefbaf72abd40bf
GITLIEN <60283203+z82206739@users.noreply.github.com>
2020-03-04 13:50:21 -0800

    Create Sum of Digits of the Number.cs

---
+++ Sum of digit of given #/Sum of Digits of the Number.cs
@@ -0,0 +1,28 @@
+static void Main(string[] args)
+        {
+            //Sum of Digits of the Number(146 = 1+4+6)
+            //Logic is to use the Number to % by 10 to get the reminder and then / 10 to move 
+            //to the next left index to get the reminder and sum them together with sum start with 
+            //the vaule of "0".
+
+            int n, k, sum = 0;
+            Console.WriteLine("Enter the number: ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                k = n % 10;
+                n = n / 10;
+                sum = sum + k;
+            }
+            Console.WriteLine($"The sum of the digits are {sum}!");
+
+        }
+       
+       
+       
+       */   n(number)     sum(total)     k(reminder)
+        146/10=14 integer   0+6=6         146%10=6
+         14/10=10           6+4=14         14%10=4
+             0              14+1=15       10%10=1
+             
